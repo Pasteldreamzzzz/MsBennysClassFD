2363f674a74aa9a37adae3b2df3a517e93f2e8d5
Pasteldreamzzzz <gabrielle.paul@patelhs.org>
2020-11-17 20:12:39 +0000

    Made the string for students

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    
+ String[] students = new String [15];
+
+
+
   }
 }
